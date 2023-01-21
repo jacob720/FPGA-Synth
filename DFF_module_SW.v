98001803fa6268e33d1be61fb4b49047b597e2f2
scotif <118193681+sc0tif@users.noreply.github.com>
2023-01-21 14:11:17 +0000

    Create DFF_module_SW.v
    The same as janes, but with two ouputs that are the same so i can create a feedback loop :)

---
+++ DFF_module_SW.v
@@ -0,0 +1,20 @@
+//slight adaptation from janes - two outputs
+
+module DFF_module_SW(
+	input D,
+	input clk,
+	
+	output reg Q,
+	output reg Q2 // for the one bit register to have a feedback loop and an output
+);
+
+	always @ (posedge clk)
+	
+	begin
+		
+		Q <= D;
+		Q2 = Q; //assigns the same for the feedback loop
+		
+	end
+	
+endmodule
