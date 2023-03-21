e6b67d0c444f4d4b16a3a9ec443139bf9fded5a9
scotif <118193681+sc0tif@users.noreply.github.com>
2023-03-21 21:54:38 +0000

    Update VariableClkSeq.v

--- VariableClkSeq.v
+++ VariableClkSeq.v
@@ -14,10 +14,10 @@ integer i =0;
 
 always@(posedge clk100hz) begin
 	
-	if (user_input0 > 10'b1110000101) begin //901:1000 - > 12 - - 40s clk
-		if ( i > 2000) begin
+	if (user_input0 > 10'b1110000101) begin //901:1000 - > 12 - - 4.0s clk
+		if ( i > 200) begin
            		variableClk = 1'b0;  
-			if (i >= 4000) begin
+			if (i >= 400) begin
 				i = 0;
 			end
 		end
@@ -28,10 +28,10 @@ always@(posedge clk100hz) begin
 	end
 	
 	
-	else if (user_input0 > 10'b1100100001) begin //801:900 -> 11 -- 35s
-		if ( i > 1750) begin
+	else if (user_input0 > 10'b1100100001) begin //801:900 -> 11 -- 3.5s
+		if ( i > 175) begin
            		variableClk = 1'b0;  
-			if (i >= 3500) begin
+			if (i >= 350) begin
 				i = 0;
 			end
 		end
@@ -41,10 +41,10 @@ always@(posedge clk100hz) begin
         end 
 	end
 		
-	else if (user_input0 > 10'b1010111101) begin //701:800 -> 10 -- 30s
-		if ( i > 1500) begin
+	else if (user_input0 > 10'b1010111101) begin //701:800 -> 10 -- 3.0s
+		if ( i > 150) begin
            		variableClk = 1'b0;  
-			if (i >= 3000) begin
+			if (i >= 300) begin
 				i = 0;
 			end
 		end
@@ -54,74 +54,7 @@ always@(posedge clk100hz) begin
         end 
 	end
 		
-	else if (user_input0 > 10'b1001011001) begin //601:700 -> 9 -- 25s
-		if ( i > 1250) begin
-           		variableClk = 1'b0;  
-			if (i >= 2500) begin
-				i = 0;
-			end
-		end
-		
-        else begin 
-        		variableClk = 1'b1;
-        end 
-	end
-	
-	else if (user_input0 > 9'b111110101) begin //501:600 -> 8 -- 20s
-		if ( i > 1000) begin
-           		variableClk = 1'b0;  
-			if (i >= 2000) begin
-				i = 0;
-			end
-		end
-		
-        else begin 
-        		variableClk = 1'b1;
-        end 
-	end
-	
-	else if (user_input0 > 9'b110010001) begin //401:500 -> 7 -- 15s
-		if ( i > 750) begin
-           		variableClk = 1'b0;  
-			if (i >= 1500) begin
-				i = 0;
-			end
-		end
-		
-        else begin 
-        		variableClk = 1'b1;
-        end 
-	end
-	
-	
-	else if (user_input0 > 9'b100101101) begin //301:400 -> 6 -- 10s
-		if ( i > 500) begin
-           		variableClk = 1'b0;  
-			if (i >= 1000) begin
-				i = 0;
-			end
-		end
-		
-        else begin 
-        		variableClk = 1'b1;
-        end 
-	end
-	
-
-	else if (user_input0 > 8'b11001001) begin //201:300 -> 5 -- 5s
-		if ( i > 250) begin
-           		variableClk = 1'b0;  
-			if (i >= 500) begin
-				i = 0;
-			end
-		end
-		
-        else begin 
-        		variableClk = 1'b1;
-        end 
-	end
-	
-	else if (user_input0 > 7'b1100101) begin //101:200 -> 4 -- 2.5s
+	else if (user_input0 > 10'b1001011001) begin //601:700 -> 9 -- 2.5s
 		if ( i > 125) begin
            		variableClk = 1'b0;  
 			if (i >= 250) begin
@@ -134,7 +67,34 @@ always@(posedge clk100hz) begin
         end 
 	end
 	
-	else if (user_input0 > 0) begin //0:  100 -> 3 -- 1s
+	else if (user_input0 > 9'b111110101) begin //501:600 -> 8 -- 2.0s
+		if ( i > 100) begin
+           		variableClk = 1'b0;  
+			if (i >= 200) begin
+				i = 0;
+			end
+		end
+		
+        else begin 
+        		variableClk = 1'b1;
+        end 
+	end
+	
+	else if (user_input0 > 9'b110010001) begin //401:500 -> 7 -- 1.5s
+		if ( i > 75) begin
+           		variableClk = 1'b0;  
+			if (i >= 150) begin
+				i = 0;
+			end
+		end
+		
+        else begin 
+        		variableClk = 1'b1;
+        end 
+	end
+	
+	
+	else if (user_input0 > 9'b100101101) begin //301:400 -> 6 -- 1s
 		if ( i > 50) begin
            		variableClk = 1'b0;  
 			if (i >= 100) begin
@@ -146,11 +106,38 @@ always@(posedge clk100hz) begin
         		variableClk = 1'b1;
         end 
 	end
+	
 
-	else begin
-		if ( i > 25) begin //catch clause, 0.5s
+	else if (user_input0 > 8'b11001001) begin //201:300 -> 5 -- 0.5s
+		if ( i > 25) begin
            		variableClk = 1'b0;  
 			if (i >= 50) begin
+				i = 0;
+			end
+		end
+		
+        else begin 
+        		variableClk = 1'b1;
+        end 
+	end
+	
+	else if (user_input0 > 7'b1100101) begin //0:200 -> 4 -- 0.25s
+		if ( i > 12) begin
+           		variableClk = 1'b0;  
+			if (i >= 25) begin
+				i = 0;
+			end
+		end
+		
+        else begin 
+        		variableClk = 1'b1;
+        end 
+	end
+
+	else begin
+		if ( i > 10) begin //catch clause -> 0.1s
+           		variableClk = 1'b0;  
+			if (i >= 5) begin
 				i = 0;
 			end
 		end
