727d6e510d5c9e84d9333174cbc7e9b1c798da6e
scotif <118193681+sc0tif@users.noreply.github.com>
2023-04-11 15:24:51 +0100

    Update SustainVariable.v

--- SustainVariable.v
+++ SustainVariable.v
@@ -1,61 +1,39 @@
 //sustain value from the user input
 //input is in the range 0-1023 from arduino
 //output range is 3-12
-
+//clk input is board clk - needs to run quickly so that it auto updates
 
 module SustainVariable(
 
- input user_input0,
+ input [9:0] user_input0,
  input clk,
- output reg [3:0] sustainTime
+ output reg [3:0] sustainTime // needs to be in the range 0-4
 
 );
 
 
 always@(posedge clk) begin
+	
+	if (user_input0 > 10'b1110000101) begin //801:1000 - > 4
+		sustainTime = 4'b0100;
+		end
+	else if (user_input0 > 10'b1010111101) begin //601:800 -> 3
+		sustainTime = 4'b0011;
+		end
+	else if (user_input0 > 9'b111110101) begin //401:600 -> 2
+		sustainTime = 4'b0010;
+		end
+	else if (user_input0 > 9'b100101101) begin //201:400 -> 1
+		sustainTime = 4'b0001;
+		end
+	else if (user_input0 > 7'b1100101) begin //101:200 -> 0
+		sustainTime = 4'b0000;
+		end
+	
+	else begin
+		sustainTime = 4'b0000; //is 0 as a fail safe
+		end
 
- if (user_input0 > 10'd1000) begin
-			sustainTime = 10'd12;
-	end
-	 
- else if (user_input0 > 10'd900) begin
-			sustainTime = 10'd11;
-	end
-	
- else if (user_input0 > 10'd700) begin
-			sustainTime = 10'd10;
-	end
-	
- else if (user_input0 > 10'd600) begin
-			sustainTime = 10'd9;
-	end
-	
- else if (user_input0 > 10'd500) begin
-			sustainTime = 10'd8;
-	end
-	
- else if (user_input0 > 10'd400) begin
-			sustainTime = 10'd7;
-	end
-	
- else if (user_input0 > 10'd300) begin
-			sustainTime = 10'd6;
-	end
-	
- else if (user_input0 > 10'd200) begin
-			sustainTime = 10'd5;
-	end
-	
- else if (user_input0 > 10'd100) begin
-			sustainTime = 10'd4;
-	end
-
-else begin 
-		sustainTime = 10'd3;
-	end
-
-
-
-end
+ end
 
 endmodule
